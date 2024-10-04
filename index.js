8076b198ae101b4c944374a0886fe0e846c4da21
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-10-04 20:49:26 +0200

    commented out code to expirement later

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+// const { DB } = require('./config');
+// const {Client} = require('pg');
 
 const { PORT } = require('./config');
 
@@ -11,10 +13,34 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* const db = new Client({
+    user: DB.PGUSER,
+    host: DB.PGHOST,
+    database: DB.PGDATABASE,
+    password: DB.PGPASSWORD,
+    port: DB.PGPORT,
+  });
+
+
+const getProducts = (req, res, next) => {
+    db.query('SELECT * FROM products', (err, result) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        } else {
+            return res.status(200).json(result.rows);
+        }
+
+    })
+}
+
+app.get('/products', getProducts); */
 
 app.use('/', (req, res, next) => {
-    res.send('Hello World!')
+    res.json({message: 'Welcome to Pleigns!'})
 })
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
