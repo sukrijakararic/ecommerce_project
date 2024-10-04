7784be745180ef12db452b325df4ce4f8ebe117f
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-10-04 19:38:27 +0200

    added some security to my server and body parser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const { PORT } = require('./config');
 
 app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use('/', (req, res, next) => {
     res.send('Hello World!')
