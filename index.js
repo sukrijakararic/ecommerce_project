b21e54b3ac09760bfba81aa04d5ac41b4de54f03
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-10-05 12:40:42 +0200

    added a pool in config files

--- index.js
+++ index.js
@@ -3,39 +3,13 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-// const { DB } = require('./config');
-// const {Client} = require('pg');
 
-const { PORT } = require('./config');
+const { PORT } = require('./config/config');
 
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-/* const db = new Client({
-    user: DB.PGUSER,
-    host: DB.PGHOST,
-    database: DB.PGDATABASE,
-    password: DB.PGPASSWORD,
-    port: DB.PGPORT,
-  });
-
-
-const getProducts = (req, res, next) => {
-    db.query('SELECT * FROM products', (err, result) => {
-        if (err) {
-            return res.status(400).json({
-                error: err
-            });
-        } else {
-            return res.status(200).json(result.rows);
-        }
-
-    })
-}
-
-app.get('/products', getProducts); */
 
 app.use('/', (req, res, next) => {
     res.json({message: 'Welcome to Pleigns!'})
