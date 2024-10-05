813cfb39ab0c5e9a844ef998d99d3ed6276b4c59
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-10-05 13:55:15 +0200

    added a get routet to get a user by id

--- queries/user.js
+++ queries/user.js
@@ -1,15 +1,29 @@
-const db = require('../db/pool');
+const db = require("../db/pool");
 
- const registerUser = async (request, response, next) => {
-    const {email, password, firstname, lastname} = request.body;
+const registerUser = async (request, response, next) => {
+  const { email, password, firstname, lastname } = request.body;
 
-    try {
-        const result = await db.query(
-            "INSERT INTO users (email, password, firstName, lastName) VALUES ($1, $2, $3, $4) RETURNING *", [email, password, firstname, lastname]
-        );
-        response.status(201).json({message: "User created", user: result.rows[0]});
-    } catch (err) {
-        console.log(err);
-    }
-}
-module.exports = registerUser
+  try {
+    const result = await db.query(
+      "INSERT INTO users (email, password, firstName, lastName) VALUES ($1, $2, $3, $4) RETURNING *",
+      [email, password, firstname, lastname]
+    );
+    response
+      .status(201)
+      .json({ message: "User created", user: result.rows[0] });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getUserById = async (request, response, next) => {
+  const id = parseInt(request.params.id);
+  try {
+    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    response.status(200).json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {registerUser, getUserById};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRouter = express.Router();
-const registerUser = require('../queries/user');
+const { registerUser, getUserById } = require('../queries/user');
 
 module.exports = (app) => {
     app.use('/users', userRouter);
 
     userRouter.post('/register', registerUser);
+
+    userRouter.get('/:id', getUserById);
 
 }
